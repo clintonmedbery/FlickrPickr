aae28ec2f757c05f5b581bc86adc420621764340
clintomed <clintonmedbery@Clintons-Mac-Pro-2.local>
2015-06-14 14:26:44 -0400

    Initial Commit

---
+++ FlickrPickr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FlickrPickr
+//
+//  Created by Clinton Medbery on 6/14/15.
+//  Copyright (c) 2015 Programadores Sans Frontieres. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
